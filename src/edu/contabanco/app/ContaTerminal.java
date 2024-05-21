3ff86acf8a995873200580f45b7e3a733f4425f4
JuliaLouback <julialoubackribeiro2001@gmail.com>
2024-05-21 17:33:40 -0300

    Initial Commit - ContaTerminal

---
+++ src/edu/contabanco/app/ContaTerminal.java
@@ -0,0 +1,35 @@
+package edu.contabanco.app;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        String nome;
+        String numeroAgencia;
+        int numeroConta;
+        double valorSaldo;
+
+        String resultado;
+
+        System.out.println("Por favor, digite seu nome:");
+        nome = scanner.nextLine();
+
+        System.out.println("Por favor, digite o número da Agência:");
+        numeroAgencia = scanner.nextLine();    
+
+        System.out.println("Por favor, digite o número da conta:");
+        numeroConta = scanner.nextInt();
+        
+        System.out.println("Por favor, digite o valor do saldo:");
+        valorSaldo = scanner.nextDouble();
+
+        resultado = String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque", nome, numeroAgencia, numeroConta, valorSaldo); 
+
+        System.out.println(resultado);
+
+    }
+    
+}
